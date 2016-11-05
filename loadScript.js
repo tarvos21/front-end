cd0661f744682a52a0e4aa168ebb6b45da69e9ea
DuBing <tarvos21@live.com>
2016-11-05 19:08:27 +0800

    Create script node and add code to it

---
+++ loadScript.js
@@ -0,0 +1,24 @@
+fucntion loadScript(url) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "url";
+    document.appendChild(script);
+}
+
+loadScript("client.js");
+
+<script type="text/javascript">
+    function sayHi() {
+        alert("Hi, dubing!");
+    }
+</script>
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+var code = "function sayHi() {alert(\"Hi, DuBing!\");}";
+try {
+    script.appendChild(document.createTextNode("code"));
+} catch (ex) {
+    script.text = "code";
+}
+document.body.appendChild(script);
