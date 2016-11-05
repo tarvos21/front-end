32806b7c48b68419b4a88c6a23e4c56f2b3fc4e4
DuBing <tarvos21@live.com>
2016-11-05 10:30:12 +0800

    Create and add attribute to element

---
+++ createAttribute.js
@@ -0,0 +1,6 @@
+var attr = document.createAttribute('align');
+attr.value = 'left';
+element.setAttributeNode(attr);
+alert(element.attributes['align'].value);    //'left'
+alert(element.getAttributeNode('align').value);     //'left'
+alert(element.getAttribute('align');    //'left'
