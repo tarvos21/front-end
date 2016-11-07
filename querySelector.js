2b7b69bc6ffd8c2c1aba4dc6b20f874ab2ca175c
DuBing <tarvos21@live.com>
2016-11-07 11:22:32 +0800

    Simple usage of querySelector

---
+++ querySelector.js
@@ -0,0 +1,11 @@
+//Get the body element
+var body = document.querySelector("body");
+
+//Get the element with id="dubing"
+var dubing = document.querySelector("#dubing")
+
+//Get the first element with class="family"
+var family = document.querySelector(".family")
+
+//Get the first image element with class="button"
+var img = document.querySelector("img.button")
